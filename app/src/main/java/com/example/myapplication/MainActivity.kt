57a3f3bef4682694afd099a08c7b4ef1fbf1483c
OrzMiku <miku@ecy.pink>
2024-10-11 14:58:08 +0800

    生命周期

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -14,8 +15,10 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.myapplication.ui.theme.MyApplicationTheme
 
 class MainActivity : ComponentActivity() {
+    private val mTag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(mTag, "onCreate Called")
         enableEdgeToEdge()
         setContent {
             MyApplicationTheme {
@@ -27,6 +30,46 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d(mTag, "onStart Called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(mTag, "onResume Called")
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(mTag, "onRestoreInstanceState Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d(mTag, "onRestart Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d(mTag, "onPause Called")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        Log.d(mTag, "onSaveInstanceState Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d(mTag, "onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(mTag, "onDestroy Called")
     }
 }
 
